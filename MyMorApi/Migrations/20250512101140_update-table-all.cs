61705adb96f4a848d438a8909a5bb58db358e9e8
Sandeep Sharma <sandeepsharma553@gmail.com>
2025-05-12 15:46:19 +0530

    changes

---
+++ MyMorApi/Migrations/20250512101140_update-table-all.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyMorApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatetableall : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Hostel",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 12, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Login",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 12, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 12, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "University",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 12, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 12, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Hostel",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 9, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Login",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 9, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 9, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "University",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 9, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 9, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+    }
+}
